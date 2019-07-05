4e646b04b760412d1a5144c837aa7225a63a4458
ardab <arkadiusz.dabkowski@agora.pl>
2019-07-05 16:54:34 +0200

    Training notebook with eager execution tensorflow.

--- courses/machine_learning/deepdive/06_structured/labs/lab_eager_execution.py
+++ courses/machine_learning/deepdive/06_structured/labs/lab_eager_execution.py
@@ -1,6 +1,26 @@
+
+#%%
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import tensorflow as tf
 
+#%%
 tf.enable_eager_execution()
 tf.executing_eagerly()
+
+
+#%%
+x = [[2.]]
+m = tf.matmul(x,x)
+print("hello, {}".format(m))
+
+#%%
+a=tf.constant([[2,3],[3,4]])
+print(a)
+
+#%%
+b = tf.add(a,1)
+print(b)
+
+
+#%%
